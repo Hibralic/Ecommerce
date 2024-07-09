02bb9a2c4a3bad4dabb75b45c1446f8587865d9a
Hibralic <hasib.ibralic@gmail.com>
2024-07-09 19:06:19 -0400

    added getBagButtons(), local storage, and getProducts()

--- app.js
+++ app.js
@@ -59,10 +59,36 @@ class UI {
     });
     productsDOM.innerHTML = result;
   }
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttons.forEach((button) => {
+      let id = button.dataset.id;
+      let inCart = cart.find((item) => item.id === id);
+      if (inCart) {
+        button.innerText = "In Cart";
+        button.disabled = true;
+      } else {
+        button.addEventListener(`click`, (event) => {
+          event.target.innerText = "In Cart";
+          event.target.disabled = true;
+          // get product from products
+          // add product to the cart
+          // save cart in local storage
+          // set cart values
+          // display cart item
+          // show the cart
+        });
+      }
+    });
+  }
 }
 
 // local storage
-class Storage {}
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+}
 //
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -70,5 +96,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const products = new Products();
 
   //    get all products
-  products.getProducts().then((products) => ui.displayProducts(products));
+  products
+    .getProducts()
+    .then((products) => {
+      ui.displayProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => {
+      ui.getBagButtons();
+    });
 });
